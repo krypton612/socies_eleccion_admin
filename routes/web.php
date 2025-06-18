cf0b53450da5789365c79dea62ff37bba0457c7e
Mona lisa <ronalddiazy@gmail.com>
2025-06-18 02:45:28 -0400

    Redirect routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::to('/control/login');
 });
+
+Route::get('/login', function () {
+    return Redirect::to('/control/login');
+})->name('login');
